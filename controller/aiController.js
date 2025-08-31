b4088fc25fe3e51404e3b565224013c069f0916b
shobhitmaurya06 <shobhitmaurya9346@gmail.com>
2025-08-31 23:01:24 +0530

    resume-review completed

--- controller/aiController.js
+++ controller/aiController.js
@@ -3,6 +3,8 @@ import sql from "../config/Database.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+import pdf from 'pdf-parse/lib/pdf-parse.js'
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -230,6 +232,50 @@ export const removeImageObject = async (req, res) => {
       VALUES (${userId},${`Removed ${object} from image`}, ${imageURl}, 'image')
     `;
     return res.json({ success: true, content:imageURl});
+  } catch (error) {
+    console.error("Generate Image Error:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const resumeReview = async (req, res) => {
+  console.log("enter in the controller");
+  try {
+    const { userId } = req.auth();
+    const resume=req.file;
+    const plan = req.plan;
+    // check plan
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for premium subscriptions"
+      });
+    }
+    //check file size
+    if(resume.size>10*1024*1024){
+      return res.json({
+        success:false,
+        message:"Resume file size exceeds  allowed size  10MB"
+      })
+    }
+    const dataBuffer=fs.readFileSync(resume.path);
+    const pdfData=await pdf(dataBuffer);
+    const prompt=`Review the following resume and provide constructive feedback on its strengths ,weakness, and areas for improvement  and calculate ATS score. Resume content:\n\n${pdfData.text}`;
+     const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.7,
+      max_tokens:1000
+    });
+    // save to db
+    const content=response.choices[0].message.content;
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type)
+      VALUES (${userId},'Review the uploaded resume', ${content}, 'resume-review')
+    `;
+    return res.json({ success: true, content:content});
   } catch (error) {
     console.error("Generate Image Error:", error.message);
     return res.status(500).json({
